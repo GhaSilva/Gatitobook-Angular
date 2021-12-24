56e3a7041d2dc7169b74b34bbda3b516e3c28668
Ghabriel Winicius <ghabriel-silva@outlook.com>
2021-12-24 18:36:03 -0300

    Login

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path:'',
-    component:HomeComponent
+    component:HomeComponent,
+    children: [
+      {
+        path:'',
+        component:LoginComponent,
+
+      },
+    ],
 
   }
 ];
