617174ff9ae7b5d0d67560f34296b48969861bde
Ghabriel Winicius <ghabriel-silva@outlook.com>
2021-12-27 16:12:44 -0300

    Validação de campos

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,3 +1,4 @@
+import { MensagemModule } from './../componentes/mensagem/mensagem.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -15,7 +16,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     HomeRoutingModule,
-    FormsModule
+    FormsModule,
+    MensagemModule,
   ],
   exports:[HomeComponent],
 })
