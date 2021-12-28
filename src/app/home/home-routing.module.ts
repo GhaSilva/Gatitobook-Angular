07a8e6906429b3cf0ba88c2dfe5e80c89ba6d162
Ghabriel Winicius <ghabriel-silva@outlook.com>
2021-12-28 15:57:21 -0300

    Cadastro do usuario, componente

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
         path:'',
         component:LoginComponent,
 
+      },
+      {
+        path:"novousuario",
+        component:NovoUsuarioComponent
       },
     ],
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,12 +6,14 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NovoUsuarioComponent
   ],
   imports: [
     CommonModule,
